49a8164a7893149a286d776e8216bd3cd46e17c4
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-20 21:12:40 +0700

    :package: add package to all files

---
+++ src/javax/microedition/lcdui/game/TiledLayer.java
@@ -0,0 +1,1 @@
+package javax.microedition.lcdui.game;
--- src/javax/microedition/lcdui/package-info.java
+++ src/javax/microedition/lcdui/package-info.java
@@ -1,4 +1,7 @@
 /**
+ * <p>
  * The UI API provides a set of features for implementation of user interfaces
  * for MIDP applications.
+ * </p>
  */
+package javax.microedition.lcdui;
