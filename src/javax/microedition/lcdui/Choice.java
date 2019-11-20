34859ff47b7f61064640d2a91605654ba2c35896
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-20 20:05:36 +0700

    fix package location

--- src/javax/microedition/lcdui/Choice.java
+++ src/javax/microedition/lcdui/Choice.java
@@ -4,5 +4,5 @@ package javax.microedition.lcdui;
  * Choice defines an API for a user interface components implementing selection from predefined number of choices.
  */
 public interface Choice {
-
+    public static int EXCLUSIVE
 }
