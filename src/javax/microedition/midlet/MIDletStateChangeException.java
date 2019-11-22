5a1424c7f97435bcdf46f5cc8e18319a54f70fa7
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-22 17:41:31 +0700

    :sparkles: add specification for MIDletStateChangeException

--- src/javax/microedition/midlet/MIDletStateChangeException.java
+++ src/javax/microedition/midlet/MIDletStateChangeException.java
@@ -1,1 +1,27 @@
 package javax.microedition.midlet;
+
+/**
+ * Signals that a requested {@code MIDlet} state change failed. This exception
+ * is thrown by the {@code MIDlet} is response to state change calls into the
+ * application via the {@code MIDlet} interface
+ * 
+ * @since MIDP 1.0
+ */
+public class MIDletStateChangeException extends Exception {
+
+    /**
+     * Constructs an exception with no specified detail message.
+     */
+    public MIDletStateChangeException() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Constructs an exception with the specified detail message.
+     * 
+     * @param s the detail message
+     */
+    public MIDletStateChangeException(String s) {
+        throw new NotImplementedException();
+    }
+}
