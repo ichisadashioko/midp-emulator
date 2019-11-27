1b175c72b044c35300a4feb56bcfd4f6a2f0889d
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-27 07:20:46 +0700

    :construction: define connection intefaces

--- src/javax/microedition/io/InputConnection.java
+++ src/javax/microedition/io/InputConnection.java
@@ -1,1 +1,30 @@
 package javax.microedition.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.DataInputStream;
+
+/**
+ * This interface defines the capabilities that an input stream connection must
+ * have.
+ * 
+ * @since CLDC 1.0
+ */
+public interface InputConnection extends Connection {
+
+    /**
+     * Open and return an input stream for a connection.
+     * 
+     * @return An input stream
+     * @throws IOException If an I/O error occurs
+     */
+    public InputStream openInputStream() throws IOException;
+
+    /**
+     * Open and return a data input stream for a connection.
+     * 
+     * @return An input stream
+     * @throws IOException If an I/O error occurs
+     */
+    public DataInputStream openDataInputStream() throws IOException;
+}
--- src/javax/microedition/io/StreamConnection.java
+++ src/javax/microedition/io/StreamConnection.java
@@ -1,1 +1,14 @@
 package javax.microedition.io;
+
+/**
+ * <p>
+ * This interface defines the capabilities that a stream connection must have.
+ * 
+In a typical implementation of this interface (for instance in MIDP 2.0), all StreamConnections have one underlying InputStream and one OutputStream. Opening a DataInputStream counts as opening an InputStream and opening a DataOutputStream counts as opening an OutputStream. Trying to open another InputStream or OutputStream causes an IOException. Trying to open the InputStream or OutputStream after they have been closed causes an IOException.
+
+The methods of StreamConnection are not synchronized. The only stream method that can be called safely in another thread is close.
+ * @since CLDC 1.0
+ */
+public interface StreamConnection extends InputConnection, OutputConnection {
+
+}
