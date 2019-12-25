895a04ed9a71310baca48f3d8c1857e513a8adf2
Ichisada Shioko <ichisadashioko@gmail.com>
2019-12-25 21:31:48 +0700

    :lock: define pki package without docs

--- src/main/java/javax/microedition/pki/Certificate.java
+++ src/main/java/javax/microedition/pki/Certificate.java
@@ -1,1 +1,19 @@
 package javax.microedition.pki;
+
+public interface Certificate {
+    public String getIssuer();
+
+    public long getNotAfter();
+
+    public long getNotBefore();
+
+    public String getSerialNumber();
+
+    public String getSigAlgName();
+
+    public String getSubject();
+
+    public String getType();
+
+    public String getVersion();
+}
--- src/main/java/javax/microedition/pki/CertificateException.java
+++ src/main/java/javax/microedition/pki/CertificateException.java
@@ -1,1 +1,36 @@
 package javax.microedition.pki;
+
+import java.io.IOException;
+
+public class CertificateException extends IOException {
+    public static final byte BAD_EXTENSIONS = 1;
+    public static final byte CERTIFICATE_CHAIN_TOO_LONG = 2;
+    public static final byte EXPIRED = 3;
+    public static final byte UNAUTHORIZED_INTERMEDIATE_CA = 4;
+    public static final byte MISSING_SIGNATURE = 5;
+    public static final byte NOT_YET_VALID = 6;
+    public static final byte SITENAME_MISMATCH = 7;
+    public static final byte UNRECOGNIZED_ISSUER = 8;
+    public static final byte UNSUPPORTED_SIGALG = 9;
+    public static final byte INAPPROPRIATE_KEY_USAGE = 10;
+    public static final byte BROKEN_CHAIN = 11;
+    public static final byte ROOT_CA_EXPIRED = 12;
+    public static final byte UNSUPPORTED_PUBLIC_KEY_TYPE = 13;
+    public static final byte VERIFICATION_FAILED = 14;
+
+    public CertificateException(Certificate certificate, byte status) {
+        throw new UnsupportedOperationException();
+    }
+
+    public CertificateException(String message, Certificate certificate, byte status) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Certificate getCertificate() {
+        throw new UnsupportedOperationException();
+    }
+
+    public byte getReason() {
+        throw new UnsupportedOperationException();
+    }
+}
