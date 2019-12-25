ba600273448dc24c24d6c67e67b341229d786368
Ichisada Shioko <ichisadashioko@gmail.com>
2019-12-25 21:24:35 +0700

    :zap: define media.control package without docs

--- src/main/java/javax/microedition/media/control/VolumeControl.java
+++ src/main/java/javax/microedition/media/control/VolumeControl.java
@@ -1,1 +1,13 @@
 package javax.microedition.media.control;
+
+import javax.microedition.media.Control;
+
+public interface VolumeControl extends Control {
+    public int getLevel();
+
+    public boolean isMuted();
+
+    public int setLevel(int level);
+
+    public void setMute(boolean mute);
+}
