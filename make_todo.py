65f87e8a55dc1beb514a874f4d330cc63c4b40aa
Ichisada Shioko <ichisadashioko@gmail.com>
2020-01-04 10:28:05 +0700

    draft CommConnection docs

--- make_todo.py
+++ make_todo.py
@@ -1,28 +1,53 @@
 package_root = './src/main/java/javax/microedition/lcdui/game/'
 
 classes = [
+    #=== javax.microedition.io
+    # 'CommConnection',
+    # 'Connection',
+    # 'ConnectionNotFoundException',
+    # 'Connector',
+    # 'ContentConnection',
+    # 'Datagram',
+    # 'DatagramConnection',
+    # 'HttpConnection',
+    # 'HttpsConnection',
+    # 'InputConnection',
+    # 'OutputConnection',
+    # 'PushRegistry',
+    # 'SecureConnection',
+    # 'SecurityInfo',
+    # 'ServerSocketConnection',
+    # 'SocketConnection',
+    # 'StreamConnection',
+    # 'StreamConnectionNotifier',
+    # 'UDPDatagramConnection',
+    #=== javax.microedition.lcdui.game
     # 'GameCanvas',
     # 'Layer',
     # 'LayerManager',
     # 'Sprite',
     # 'TiledLayer',
-    # 'RecordComparator',
-    # 'RecordEnumeration',
-    # 'RecordFilter',
-    # 'RecordListener',
-    # 'RecordStore',
-    # 'InvalidRecordIDException',
-    # 'RecordStoreException',
-    # 'RecordStoreFullException',
-    # 'RecordStoreNotFoundException',
-    # 'RecordStoreNotOpenException',
+    #=== javax.microedition.rms
+    'RecordComparator',
+    'RecordEnumeration',
+    'RecordFilter',
+    'RecordListener',
+    'RecordStore',
+    'InvalidRecordIDException',
+    'RecordStoreException',
+    'RecordStoreFullException',
+    'RecordStoreNotFoundException',
+    'RecordStoreNotOpenException',
+    #=== javax.microedition.midlet
     # 'MIDlet',
     # 'MIDletStateChangeException',
+    #=== javax.microedition.media.control
     # 'ToneControl',
     # 'VolumeControl',
-    'Certificate',
-    'CertificateException',
+    #=== javax.microedition.pki
+    # 'Certificate',
+    # 'CertificateException',
 ]
 
 for c in classes:
-    print(f'- [ ] [`{c}`]({package_root}{c}.java)')
+    print(f'- [ ] `{c}`')
--- src/main/java/javax/microedition/io/CommConnection.java
+++ src/main/java/javax/microedition/io/CommConnection.java
@@ -1,5 +1,8 @@
 package javax.microedition.io;
 
+/**
+ * <P>This interface defines a logical serial port connection. A "logical" serial port is defined as a logical connection through which bytes are transferring serially. The logical serial port is defined within the underlying operating system and may not necessarily correspond to a physical RS-232 serial port. For instance, IrDA IRCOMM ports can commonly be configured as a logical serial port within the operating system so that it 
+ */
 public interface CommConnection extends StreamConnection {
     
 }
