51f2a82074ff85fca0a8fa18e687816b3042f642
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-13 20:07:26 +0700

    StreamConnectionNotifier: complete doc

--- src/main/java/javax/microedition/io/StreamConnectionNotifier.java
+++ src/main/java/javax/microedition/io/StreamConnectionNotifier.java
@@ -1,5 +1,20 @@
 package javax.microedition.io;
 
+import java.io.IOException;
+
+/**
+ * This interface defines the capabilities that a connection notifier must have.
+ * 
+ * @since CLDC 1.0
+ */
 public interface StreamConnectionNotifier extends Connection {
-    
+
+    /**
+     * Returns a {@code StreamConnection} object that represents a server side
+     * socket connection. The method blocks until a connection is made.
+     * 
+     * @return A {@code StreamConnection} to communicate with a client.
+     * @throws IOException If an I/O error occurs.
+     */
+    public StreamConnection acceptAndOpen() throws IOException;
 }
