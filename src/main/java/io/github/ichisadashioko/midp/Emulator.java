cf3b2bda40cef87f2cc889f758fa54842662cdf3
Ichisada Shioko <ichisadashioko@gmail.com>
2020-09-12 01:06:10 -0400

    listen to mouse events

--- src/main/java/io/github/ichisadashioko/midp/Emulator.java
+++ src/main/java/io/github/ichisadashioko/midp/Emulator.java
@@ -1,8 +1,29 @@
 package io.github.ichisadashioko.midp;
 
 import java.awt.Frame;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
-class MainWindow extends Frame {
+class MainWindow extends Frame implements MouseListener {
+    public void mouseClicked(MouseEvent e) {
+        System.out.println(e);
+    }
+
+    public void mousePressed(MouseEvent e) {
+        System.out.println(e);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        System.out.println(e);
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        System.out.println(e);
+    }
+
+    public void mouseExited(MouseEvent e) {
+        System.out.println(e);
+    }
 }
 
 public class Emulator {
@@ -10,6 +31,7 @@ public class Emulator {
         System.out.println("Hello");
 
         MainWindow window = new MainWindow();
+        window.addMouseListener(window);
         window.setSize(640, 480);
         window.setVisible(true);
     }
