a20e0612105c59f3fc4a0a58dfdae8f80025b3d4
Ichisada Shioko <ichisadashioko@gmail.com>
2020-09-12 00:58:31 -0400

    create a GUI window with AWT
    - the window cannot be close with the "close button"

--- src/main/java/io/github/ichisadashioko/midp/Emulator.java
+++ src/main/java/io/github/ichisadashioko/midp/Emulator.java
@@ -1,7 +1,16 @@
 package io.github.ichisadashioko.midp;
 
+import java.awt.Frame;
+
+class MainWindow extends Frame {
+}
+
 public class Emulator {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        MainWindow window = new MainWindow();
+        window.setSize(640, 480);
+        window.setVisible(true);
     }
 }
